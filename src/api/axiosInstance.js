c6cf189a09e4bcb9d887644f57ba53571f88dc63
priyabdj <bhardwajpriya402@gmail.com>
2025-07-02 01:12:48 +0530

    updated code

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
 
-  // baseURL: "https://glossbeauty-ecommerce.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://glossbeauty-ecommerce.onrender.com",
+  // baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 export default axiosInstance;
